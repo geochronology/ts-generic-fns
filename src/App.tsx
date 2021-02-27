250d7d4933244281a8d4c865f43af4d4e795e07e
geochronology <54387644+geochronology@users.noreply.github.com>
2021-02-26 19:29:50 -0800

    completed initial project setup with basic examples and mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import widgets from "./mock-data/widgets";
+import people from "./mock-data/people";
+
 
 function App() {
   return (
-    <div className="App">
-      hi :3
-    </div>
+    <>
+      <h2>Widgets:</h2>
+      {widgets.map(widget => {
+        return <h3>{widget.title}</h3>
+      })}
+      <h2>People:</h2>
+      {people.map(person => {
+        return <h3>{person.firstName} {person.lastName}</h3>
+      })}
+    </>
   );
 }
 
--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -1,4 +1,6 @@
-interface IFooBar {
+
+
+export interface IFooBar {
   foo: string,
   bar: string
 }
@@ -28,6 +30,89 @@ function sortByFoo(fooBars: Array<IFooBar>) {
     return 0
   })
 }
+
+
+function sortByKey<T>(data: Array<T>, key: keyof T) {
+  data.sort((a, b) => {
+    if (a[key] > b[key]) {
+      return 1
+    }
+    if (a[key] < b[key]) {
+      return -1
+    }
+    return 0
+  })
+}
+
+
+// Both fine: foo and bar are properties of IFooBar
+sortByKey<IFooBar>(fooBars, "foo");
+sortByKey<IFooBar>(fooBars, "bar");
+
+// TS complains: cat is not a property of IFooBar >:O
+// sortByKey<IFooBar>(fooBars, "cat") /* will not work due to lack of matching key */
+
+class Animal {
+  public legCount: number;
+  constructor(legCount: number) {
+    this.legCount = legCount
+  }
+}
+
+class Cat extends Animal {
+  constructor() {
+    super(4)
+  }
+}
+
+class Kangaroo extends Animal {
+  constructor() {
+    super(2)
+  }
+}
+
+class Bacteria {
+
+}
+
+// Accept any type as long as the type extends Animal
+function printLegCount<T extends Animal>(animal: T) {
+  console.log(`My leg count is: ${animal.legCount}`)
+}
+
+const myCat = new Cat()
+
+const myKangaroo = new Kangaroo()
+
+printLegCount(myCat)
+
+printLegCount(myKangaroo)
+
+const myBacteria = new Bacteria()
+
+// TS will complain:
+// bacteria does not extend animal and therefore
+// does not satisfy the generic constraint
+
+// printLegCount(myBacteria)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
